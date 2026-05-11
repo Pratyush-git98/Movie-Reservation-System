ea5836fbf6cbebbf16b99ea7ed8509cd399b6759
Pratyush verma <pratyushdv2468@gmail.com>
2026-05-11 20:23:19 +0530

    Add JWT authentication and role-based access control to movie and theatre routes

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -5,14 +5,16 @@ import {
     updateMovie,
     deleteMovie
 } from '../controllers/movie.controller.js';
+import { roleMiddleware } from '../middlewares/role.middleware.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 import { Router } from 'express';
 
 const router = Router();
 
-router.post('/', createMovie);
 router.get('/', getAllMovies);
 router.get('/:id', getMovieById);
-router.put('/:id', updateMovie);
-router.delete('/:id', deleteMovie);
+router.post('/', verifyJWT, roleMiddleware('admin'), createMovie);
+router.put('/:id', verifyJWT, roleMiddleware('admin'), updateMovie);
+router.delete('/:id', verifyJWT, roleMiddleware('admin'), deleteMovie);
 
 export default router;
--- src/routes/theatre.route.js
+++ src/routes/theatre.route.js
@@ -5,14 +5,16 @@ import {
     updateTheater,
     deleteTheater
 } from '../controllers/theatre.controller.js';
+import { roleMiddleware } from '../middlewares/role.middleware.js';
+import {verifyJWT} from '../middlewares/auth.middleware.js';
 import { Router } from 'express';
 
 const router = Router();
 
-router.post('/', createTheater);
 router.get('/', getAllTheaters);
 router.get('/:id', getTheaterById);
-router.put('/:id', updateTheater);
-router.delete('/:id', deleteTheater);
+router.post('/', verifyJWT, roleMiddleware('admin'), createTheater);
+router.put('/:id', verifyJWT, roleMiddleware('admin'), updateTheater);
+router.delete('/:id', verifyJWT, roleMiddleware('admin'), deleteTheater);
 
 export default router;
